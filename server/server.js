fe2c50738b14a9a5cc2abd64ac8343d56a744156
Priyanshu Solanki <21Bcs11551@cuchd.in>
2025-07-23 16:37:41 +0530

    Fix: CORS for www.innvibs.com domain

--- server/server.js
+++ server/server.js
@@ -10,8 +10,8 @@ const app = express();
 
 
 app.use(cors({
-    origin: 'https://innvibs.com/'
-}));
+    origin: 'https://www.innvibs.com' 
+}))
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
