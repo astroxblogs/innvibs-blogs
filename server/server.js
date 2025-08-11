bc5bc5a036538cf638354517c1170f8db7b085a6
Priyanshu Solanki <21Bcs11551@cuchd.in>
2025-08-11 14:58:49 +0530

    Redeploy the backend

--- server/server.js
+++ server/server.js
@@ -46,6 +46,6 @@ mongoose.connect(process.env.MONGO_URI)
     })
     .catch(err => console.error('MongoDB connection error:', err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
